ad81381d845d355bc656eaee0daaeeebf69e5d0a
paulsilcock <paulsilcock@users.noreply.github.com>
2022-10-14 01:35:30 +0100

    Dev/add dvc remote bucket (#6)
    Create storage bucket to use as DVC remote

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,6 +47,10 @@ resource "google_container_cluster" "main" {
       maximum       = 24
     }
   }
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
 }
 
 resource "google_container_node_pool" "main_spot_nodes" {
@@ -74,6 +78,10 @@ resource "google_container_node_pool" "main_spot_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
   }
 
   timeouts {
@@ -112,6 +120,10 @@ resource "google_container_node_pool" "gpu_spot_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
   }
 
   timeouts {
@@ -141,13 +153,96 @@ data "kubectl_file_documents" "namespaces" {
   content = file("../manifests/namespaces.yaml")
 }
 
-data "kubectl_file_documents" "argocd" {
-  content = file("../manifests/install-argocd.yaml")
-}
-
 resource "kubectl_manifest" "namespaces" {
   count     = length(data.kubectl_file_documents.namespaces.documents)
   yaml_body = element(data.kubectl_file_documents.namespaces.documents, count.index)
+}
+
+data "google_service_account" "terraform-sa" {
+  account_id = "terraform"
+}
+
+# Allow terraform service account to specify IAM policies on storage buckets
+resource "google_project_iam_binding" "terraform-binding" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  members = [
+    "serviceAccount:${data.google_service_account.terraform-sa.email}"
+  ]
+}
+
+resource "google_storage_bucket" "dvcremote" {
+  depends_on = [
+    google_project_iam_binding.terraform-binding
+  ]
+  name                        = "dvcremote-pauljs-io"
+  location                    = var.region
+  uniform_bucket_level_access = true
+}
+
+# Create GSA, KSA and bind them
+resource "google_service_account" "dvc-gsa" {
+  account_id   = "dvc-remote"
+  display_name = "DVC remote access"
+}
+resource "google_service_account_iam_binding" "workload_identity_binding" {
+  service_account_id = google_service_account.dvc-gsa.id
+  role    = "roles/iam.workloadIdentityUser"
+  members = [
+    "serviceAccount:${var.project_id}.svc.id.goog[dev/dvc-remote]"
+  ]
+}
+
+resource "kubectl_manifest" "ksa-binding" {
+  depends_on = [
+    kubectl_manifest.namespaces
+  ]
+  yaml_body = <<YAML
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    iam.gke.io/gcp-service-account: dvc-remote@${var.project_id}.iam.gserviceaccount.com
+  name: dvc-remote
+  namespace: dev
+YAML
+}
+
+# Policy to allow service account access to bucket
+data "google_iam_policy" "dvc-bucket-access" {
+  binding {
+    role = "roles/storage.objectViewer"
+    members = [
+      "serviceAccount:${google_service_account.dvc-gsa.email}"
+    ]
+  }
+
+  binding {
+    role = "roles/storage.objectCreator"
+    members = [
+      "serviceAccount:${google_service_account.dvc-gsa.email}"
+    ]
+  }
+
+  binding {
+    role = "roles/storage.legacyBucketReader"
+    members = [
+      "serviceAccount:${google_service_account.dvc-gsa.email}"
+    ]
+  }
+}
+
+# Bind policy to bucket
+resource "google_storage_bucket_iam_policy" "policy" {
+  depends_on = [
+    google_project_iam_binding.terraform-binding
+  ]
+  bucket      = google_storage_bucket.dvcremote.name
+  policy_data = data.google_iam_policy.dvc-bucket-access.policy_data
+}
+
+data "kubectl_file_documents" "argocd" {
+  content = file("../manifests/install-argocd.yaml")
 }
 
 data "kubectl_file_documents" "certmanager" {
