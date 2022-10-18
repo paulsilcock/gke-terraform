f77fe0ade973953c8dcc2da492c57d7d57540dc8
Paul Silcock <ps88uk@gmail.com>
2022-10-18 16:38:47 +0100

    Reduced provisioned disk size

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,8 @@ resource "google_container_node_pool" "main_spot_nodes" {
     preemptible  = true
     machine_type = "e2-highmem-2"
 
+    disk_size_gb = 20
+
     service_account = google_service_account.main.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
@@ -97,6 +99,8 @@ resource "google_container_node_pool" "gpu_spot_nodes" {
   node_config {
     preemptible  = true
     machine_type = "n1-standard-1"
+
+    disk_size_gb = 20
 
     guest_accelerator {
       type  = "nvidia-tesla-t4"
