d3bbc1d70a880602bdc8bd272de5395f9ca4121c
Paul Silcock <ps88uk@gmail.com>
2022-11-01 18:50:14 +0000

    Fix helm provider cluster CA

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -23,8 +23,10 @@ provider "kubectl" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.google_container_cluster.main.endpoint
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = data.google_container_cluster.main.master_auth[0].cluster_ca_certificate
+    host  = data.google_container_cluster.main.endpoint
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.main.master_auth[0].cluster_ca_certificate
+    )
   }
 }
