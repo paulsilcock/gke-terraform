4ccb29df72dba4efec1f0470031646fb208da221
Paul Silcock <ps88uk@gmail.com>
2022-11-04 04:45:15 +0000

    Ensures ArgoCD is installed before root app

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -17,3 +17,15 @@ resource "kustomization_resource" "argocd" {
 
   manifest = data.kustomization_build.argocd.manifests[each.value]
 }
+
+data "kubectl_file_documents" "rootapp" {
+  content = file("../manifests/root-app.yaml")
+}
+
+resource "kubectl_manifest" "rootapp" {
+  depends_on = [
+    kustomization_resource.argocd
+  ]
+  count     = length(data.kubectl_file_documents.rootapp.documents)
+  yaml_body = element(data.kubectl_file_documents.rootapp.documents, count.index)
+}
