3209c8a656f96696b6e0807274ed841d0b94bd7c
paulsilcock <paulsilcock@users.noreply.github.com>
2024-12-06 15:14:22 +0000

    Update provider.tf
    Fix host URL

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -22,7 +22,7 @@ provider "kubectl" {
 }
 
 provider "kubernetes" {
-  host  = data.google_container_cluster.main.endpoint
+  host  = "https://${data.google_container_cluster.main.endpoint}"
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.main.master_auth[0].cluster_ca_certificate
